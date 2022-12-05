a22ca12276b357a4c0b2e3f469c12ddd1a93c854
mchljams <mike.pisula@gmail.com>
2022-12-05 20:17:53 +0000

    put gitpod config back at root

--- packages/dinghy/src/Install.php
+++ packages/dinghy/src/Install.php
@@ -14,19 +14,8 @@ class Install {
      */
     public function execute()
     {
-        $this->publishGitPod();
         $this->publishDockerCompose();
         $this->publishConfig();
-    }
-
-    public function publishGitPod()
-    {
-        // get the contents of the docker-compose
-        $this->content = file_get_contents('./vendor/integrity/dinghy/stubs/gitpod.yml');
-
-
-        // write the docker-compose to the project root
-        file_put_contents('./gitpod.yml', $this->content);  
     }
 
     public function publishDockerCompose()
